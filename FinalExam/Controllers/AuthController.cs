7f479844a8fe7cdeee1ca400cc859e82185cc438
Karolis Tarasevičius <4karolis.t@gmail.com>
2022-10-27 19:19:10 +0300

    AuthCtrl > GetUserInfo not working. Need to fix DB?

--- FinalExam/Controllers/AuthController.cs
+++ FinalExam/Controllers/AuthController.cs
@@ -61,6 +61,27 @@ namespace FinalExam.Controllers
             var user = await _userService.GetUserByIdAsync(id);
             return Ok(user);
         }
-
+        [HttpGet("GetUserInfo")]
+        [Authorize]
+        public async Task<IActionResult> GetUserInfo()
+        {
+            var userId = int.Parse(User.Claims.FirstOrDefault(u => u.Type == ClaimTypes.NameIdentifier).Value);
+            var user = await _userService.GetUserByIdAsync(userId);
+            var infoToReturn = new UserGetDto
+            {
+                Username = user.Username,
+                Role = user.Role,
+                Name = user.PersonalInfo.Name,
+                Lastname = user.PersonalInfo.Lastname,
+                PersonalCode = user.PersonalInfo.PersonalCode,
+                Phone = user.PersonalInfo.Phone,
+                Email = user.PersonalInfo.Email,
+                City = user.PersonalInfo.ResidentialInfo.City,
+                StreetName = user.PersonalInfo.ResidentialInfo.StreetName,
+                HouseNumber = user.PersonalInfo.ResidentialInfo.HouseNumber.ToString(),
+                ApartmentNumber = user.PersonalInfo.ResidentialInfo.ApartmentNumber.ToString()
+            };
+            return Ok(infoToReturn);
+        }
     }
 }
